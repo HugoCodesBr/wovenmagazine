63609a8d5cc0ab777587a06e90f2b89ae47cb0b3
Your Name <herbtyh@gmail.com>
2020-10-20 17:59:42 -0300

    Animar menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,27 @@
-console.log('\'Allo \'Allo!');
+const lineTop = document.querySelector('.top');
+const lineMid = document.querySelector('.middle');
+const lineBot = document.querySelector('.bottom');
+const menu = document.querySelector('.menu');
+const menuClose = document.querySelector('.menu-close');
+const popupMenu = document.querySelector('.popup-menu');
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+// Animação botao do menu
+menu.addEventListener('click', function(){
+
+  if (menu.className.indexOf('menu-close') != -1){
+    lineTop.classList.remove('top-rotate');
+    lineMid.classList.remove('mid-hidden');
+    lineBot.classList.remove('bot-rotate');
+
+    popupMenu.classList.remove('popup-menu-open');
+    menu.classList.remove('menu-close');
+  }else {
+    lineTop.classList.add('top-rotate');
+    lineMid.classList.add('mid-hidden');
+    lineBot.classList.add('bot-rotate');
+
+    popupMenu.classList.add('popup-menu-open');
+    menu.classList.add('menu-close');
+  }
+});
